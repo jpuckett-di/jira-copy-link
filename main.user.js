57a0230372d4113efbd00c59db597c8d55c74238
Jeff Puckett <jpuckett@dealerinspire.com>
2024-07-19 20:31:24 +0000

    add copy link button

---
+++ main.user.js
@@ -0,0 +1,58 @@
+// ==UserScript==
+// @name        JIRA Copy Title Links
+// @namespace   Violentmonkey Scripts
+// @match       https://carscommerce.atlassian.net/*
+// @grant       none
+// @version     1.0
+// @author      -
+// @description 7/19/2024, 2:08:47 PM
+// ==/UserScript==
+const BUTTON_ID = "jira-copy-button";
+
+async function setClipboard(text) {
+  const type = "text/html";
+  const blob = new Blob([text], { type });
+  const data = [new ClipboardItem({ [type]: blob })];
+  await navigator.clipboard.write(data);
+  animateButton();
+}
+
+function animateButton() {
+  const animation = [{ transform: "scale(0.5)" }];
+
+  const timing = {
+    duration: 100,
+    iterations: 1,
+  };
+
+  document.getElementById(BUTTON_ID).animate(animation, timing);
+}
+
+function makeLink(text, url) {
+  return `<a href="${url}">${text}</a>`;
+}
+
+function findTitle() {
+  return document.querySelector(
+    'h1[data-testid="issue.views.issue-base.foundation.summary.heading"]'
+  ).textContent;
+}
+
+function findUrl() {
+  return document.querySelector(
+    'a[data-testid="issue.views.issue-base.foundation.breadcrumbs.current-issue.item"]'
+  ).href;
+}
+
+function createButton(listener) {
+  const button = document.createElement("button");
+  button.id = BUTTON_ID;
+  button.style = "position: absolute; top: 0px; left: 0px; z-index: 1000;";
+  button.textContent = "copy";
+  document.body.prepend(button);
+  button.addEventListener("click", listener, false);
+}
+
+createButton(() => {
+  setClipboard(makeLink(findTitle(), findUrl()));
+});
